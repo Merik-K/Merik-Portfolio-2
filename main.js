447daaf1a11aef642f2ecb1e584b04bf3b189725
MerikKuhn <mkuhn181@west-mec.org>
2023-04-20 18:11:14 -0700

    adding portfoilo

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+$(window).on('load', function () {
+  $('.listOptions').css('display', 'none');
+  $('.homePage').css('display', 'none');
+  $('.aboutPage').css('display', 'none');
+});
 $('.openButton').on('click', function () {
-  $(this).animate({ width: '10000px' }, 1000);
+  $(this).fadeOut(500);
+  $('.listOptions').delay(500).fadeIn(500);
+});
+$('.homeButton').on('click', function () {
+  $('.listOptions').fadeOut(500);
+  $('.homePage').delay(500).css('display', 'flex');
+});
+$('.aboutButton').on('click', function () {
+  $('.listOptions').fadeOut(500);
+  $('.aboutPage').delay(500).css('display', 'block');
+});
+$('.projectButton').on('click', function () {
+  $('.listOptions').fadeOut(500);
+  $('.homePage').delay(500).css('display', 'flex');
+});
+$('.resumeButton').on('click', function () {
+  $('.listOptions').fadeOut(500);
+  $('.homePage').delay(500).css('display', 'flex');
 });
