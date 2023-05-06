61652d051d97ade55c393df06981441cb61b8de6
MerikKuhn <mkuhn181@west-mec.org>
2023-05-05 18:20:07 -0700

    adding some more html

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 // To hide all the pages from the start and make no collison
 $(window).on('load', function () {
   $('.start').css('display', 'none');
-  $('.experiencePage').css('display', 'none');
-  $('.aboutPage').css('display', 'none');
+  $('.experience').css('display', 'none');
+  $('.about').css('display', 'none');
+  $('.projects').css('display', 'none');
+  $('.contact').css('display', 'none');
 });
 
 // Makes the open button disappear and makes list's and face
 $('.openButton').on('click', function () {
   $(this).fadeOut(500);
   $('.start').delay(500).fadeIn(500).css('display', 'block');
+});
+
+//Makes the About section on appear or onclick
+$(window).on('scroll', function () {
+  $('.about').fadeIn(100);
+});
+$('.aboutButton').on('click', function(){
+  $('.about').css('display', 'flex')
 });
