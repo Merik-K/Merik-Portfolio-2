86ec07f46201b594090cbcdca5fc5449a1322c23
MerikKuhn <mkuhn181@west-mec.org>
2023-05-12 18:22:19 -0700

    fixing animation glitches

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 // To hide all the pages from the start and make no collison
 $(window).on('load', function () {
-  $('.start').css('display', 'none');
+  // $('.start').css('display', 'none');
   $('.experience').css('display', 'none');
-  $('.about').css('display', 'none');
+  $('.about').css('display', 'hidden');
   $('.projects').css('display', 'none');
   $('.contact').css('display', 'none');
 });
 
 // Makes the open button disappear and makes list's and face
 $('.openButton').on('click', function () {
-  $(this).fadeOut(500);
-  $('.start').delay(500).fadeIn(500).css('display', 'block');
+  $(this).fadeOut(0);
+  $('.start').slideUp(10).delay(1000).fadeIn(500).css('display', 'flex', 0);
 });
 
 //Makes the About section on appear or onclick
 $(window).on('scroll', function () {
-  $('.about').fadeIn(1000);
+  $('.about').css('display', 'flex', 2000).slideDown(5000).stop();
 });
 $('.aboutButton').on('click', function () {
   $('.about').css('display', 'flex');
